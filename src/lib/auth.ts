f600560ee3296bb98657e1c95216ea35ae2320c8
Hakibati Team <contact@hakibati.dz>
2026-08-14 00:57:40 +0100

    Fix auth cookies for production

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,9 +4,21 @@ import bcrypt from 'bcryptjs';
 import { prisma } from './db';
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: 'jwt',
     maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: true,
+      },
+    },
   },
   pages: {
     signIn: '/login',
