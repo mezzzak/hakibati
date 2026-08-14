95327421bbe345dede4f3691e559362cdb3f07c2
Hakibati Team <contact@hakibati.dz>
2026-08-14 01:02:54 +0100

    Remove custom cookie config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,17 +9,6 @@ export const authOptions: NextAuthOptions = {
     strategy: 'jwt',
     maxAge: 30 * 24 * 60 * 60, // 30 days
   },
-  cookies: {
-    sessionToken: {
-      name: `__Secure-next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: 'lax',
-        path: '/',
-        secure: true,
-      },
-    },
-  },
   pages: {
     signIn: '/login',
     error: '/login',
